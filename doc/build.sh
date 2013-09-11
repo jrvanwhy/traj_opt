44874df9874fe19d30abdc21ba82fe846e2609c7
Ryan Van Why <jrvanwhy@gmail.com>
2013-09-10 17:50:50 -0700

    Making the README independent of the main documentation.

--- doc/build.sh
+++ doc/build.sh
@@ -21,19 +21,3 @@ echo "Found pdflatex at: $PDFLATEX"
 # Create the PDF of the documentation
 echo "Compiling LaTeX documentation into a PDF"
 $PDFLATEX documentation.tex
-
-# Generate README.md from the LaTeX sources using pandoc
-# If pandoc fails to read the latex, then we should switch it to read from
-# the PDF instead
-# Search for pandoc
-PANDOC=`which pandoc`
-if [ "x$PANDOC" == "x" ]
-then
-	# We did not find pandoc. Inform the user of this fact.
-	echo "Could not find pandoc, unable to update README.md"
-fi
-echo "Found pandoc at: $PANDOC"
-
-# Create the readme
-echo "Compiling LaTeX documentation into README.md"
-$PANDOC -f latex documentation.tex -t markdown_github -o ../README.md
