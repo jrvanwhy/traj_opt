5ab9a53dbb4f2bda48d35a916a516169bcb8410c
Johnathan Van Why <jrvanwhy@gmail.com>
2013-09-15 12:26:55 -0700

    More documentation work. Improved error handling in documentation build script.

--- doc/build.sh
+++ doc/build.sh
@@ -4,7 +4,7 @@
 # root folder of this repository).
 
 # Abort upon encountering an error
-set -e
+#set -e
 
 # Make sure we're in the right directory
 cd `dirname $0`
@@ -14,7 +14,10 @@ PDFLATEX=`which pdflatex`
 if [ "x$PDFLATEX" == "x" ]
 then
 	# We did not find pdflatex. Spit out an error to the console and exit.
-	echo 1>&2 "Could not find pdflatex! Aborting."
+	echo 1>&2 "Could not find pdflatex! Documentation build cannot continue without pdflatex."
+	echo On Ubuntu-based distributions, installing the texlive-latex-recommended package will bring in
+	echo all the necessary dependencies to build the documentation for this project.
+	exit
 fi
 echo "Found pdflatex at: $PDFLATEX"
 
