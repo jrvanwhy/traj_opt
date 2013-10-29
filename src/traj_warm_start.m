38626e34c08b18fe79b84a1d97ed49bb28220170
Johnathan Van Why <jrvanwhy@gmail.com>
2013-10-28 17:49:14 -0700

    Removing more unneeded files.

--- src/traj_warm_start.m
+++
@@ -1,5 +0,0 @@
-% This function sets up the scenario to be optimized again (perhaps with different additional parameters)
-
-function scenario = traj_warm_start(scenario)
-	scenario.params.init_params = scenario.minimum(:,end);
-end
